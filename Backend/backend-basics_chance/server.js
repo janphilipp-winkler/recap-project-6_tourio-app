f43848596c26f38827a095988bca15bfeb162caa
YavorLalev <yavorlalev@abv.bg>
2024-02-02 16:54:22 +0100

    Challenge Backend-basics_chance solved

--- Backend/backend-basics_chance/server.js
+++ Backend/backend-basics_chance/server.js
@@ -1,3 +1,18 @@
 import { createServer } from "node:http";
+import Chance from "chance";
 
-// export const server = …
+const chance = new Chance();
+const name = chance.name();
+const age = chance.age();
+const profession = chance.profession();
+
+export const server = createServer((request, response) => {
+  if (request.url === "/test-random-person") {
+    response.end(
+      `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.`
+    );
+  } else {
+    response.statusCode = "404";
+    response.end("Sorry! Not Found");
+  }
+});
