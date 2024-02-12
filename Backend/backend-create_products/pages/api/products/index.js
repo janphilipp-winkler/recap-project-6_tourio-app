31d41a6f6e680f933cbbef44c796868a925f0e11
YavorLalev <yavorlalev@abv.bg>
2024-02-12 11:39:33 +0100

    Fetch new POST route and send data to  database

--- Backend/backend-create_products/pages/api/products/index.js
+++ Backend/backend-create_products/pages/api/products/index.js
@@ -4,6 +4,17 @@ import Product from "../../../db/models/Product";
 export default async function handler(request, response) {
   await dbConnect();
 
+  if (request.method === "POST") {
+    try {
+      const productData = request.body;
+      await Product.create(productData);
+
+      response.status(201).json({ status: "Product created" });
+    } catch (error) {
+      response.status(400).json({ error: error.message });
+    }
+  }
+
   if (request.method === "GET") {
     const products = await Product.find();
     return response.status(200).json(products);
